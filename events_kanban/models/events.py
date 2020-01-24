792e8e7d52aee576cff103471d0721e9f40bdf49
Gabriela Rivero <regaby@gmail.com>
2020-01-24 13:41:45 -0300

    [UPD] stages in events registration kanban view

--- events_kanban/models/events.py
+++ events_kanban/models/events.py
@@ -29,4 +29,21 @@ class Event(models.Model):
 class EventRegistration(models.Model):
     _inherit = 'event.registration'
 
+    def _get_default_stage_id(self):
+        """ Gives default stage_id """
+        return self.env['event.stage'].search([('sequence', '=', '0')])
+
+    @api.model
+    def _read_group_stage_ids(self, stages, domain, order):
+        search_domain = [('id', 'in', stages.ids)]
+        stage_ids = stages._search([], order=order, access_rights_uid=SUPERUSER_ID)
+        return stages.browse(stage_ids)
+
+    # group = fields.One2many("event.event", "stage_id", "Etapa")
+    stage_id = fields.Many2one(
+        "event.stage",
+        string="Stage",
+        default=_get_default_stage_id,
+        group_expand='_read_group_stage_ids',
+    )
     color = fields.Integer('Kanban Color Index')
