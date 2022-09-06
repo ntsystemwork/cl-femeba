27d5d28ddd20618f048c4ed27e448b3230323e11
Matias <megroschopf@gmail.com>
2022-09-06 09:37:17 -0300

    ADD revierto cambios al modulo event website y agrego modulo para cambiar la inscripcion a eventos

---
+++ event_website_fix/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Fix Formulario de preinscripción para Femeba',
+    'version': '11.0.0',
+    'author': 'NTSW',
+    'website': 'www.ntsw.com.ar',
+    'license': 'AGPL-3',
+    'data': [
+    ],
+    'demo': [],
+    'images': [
+    ],
+    'depends': [
+        'website_event_sale'
+    ],
+    'installable': True,
+}
